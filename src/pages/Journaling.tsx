de42eba5e24aeddc3a2df548dae5b1f9dfc2b513
thuvohoanganh <thuvo@kse.kaist.ac.kr>
2024-05-20 15:43:09 +0900

    without chatbot

--- src/pages/Journaling.tsx
+++ src/pages/Journaling.tsx
@@ -27,7 +27,7 @@ const Landing: React.FC = () => {
     const [editDiaryIndex, setEditDiaryIndex] = useState<number>(-1)
     const [loading, setLoading] = useState(false)
     const [state, setState] = useState<number>(0)
-    const [isMuted, setIsMuted] = useState<boolean>(false)
+    const [isMuted, setIsMuted] = useState<boolean>(true)
 
     const navigate = useNavigate();
 
@@ -181,7 +181,8 @@ return (
                 <div className="flex justify-between items-center">
                     <p className="font-semibold text-4xl">My Diary</p>
                 </div>
-                <div className="pt-6 flex mt-2">
+                <p className="font-semibold text-2xl mt-6">How was your day?</p>
+                {/* <div className="pt-6 flex mt-2">
                     <div className="rounded-full h-6 w-6 overflow-hidden mr-2">
                         <img src="/assets/avatar.jpg" alt="" className="" />
                     </div>
@@ -198,7 +199,7 @@ return (
                         </div> : null
                         }
                     </div>
-                </div>
+                </div> */}
 
                 <div className="mt-4 relative">
                     <TextArea
